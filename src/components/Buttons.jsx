a6519e43d38845237ba4a4df9f4b598e090a998b
Anna Valerio <anna.j.neff@gmail.com>
2024-08-21 15:19:24 -0500

    Initial push on GA6

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,18 @@
+import satData from "./satData";
+import "../App";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
-  );
+  {displaySats.map((sat, id) => {
+    return (
+      <div>
+        <button onClick={()=>filterByType(sat)} key={id}>
+          {sat} Orbit
+          </button>
+      </div>
+    );
+  })}
+
+  <button onClick={()=>setSat(satData)}>All Orbits</button>
 };
 
 export default Buttons;
